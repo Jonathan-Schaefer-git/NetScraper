3467cb1dc8b2c37a872f8db099b22fbc6921ea10
Jona4Dev <71170472+Jona4Play@users.noreply.github.com>
2022-11-16 20:08:35 +0100

    Delete NetScraper directory

--- NetScraper/ImageData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NetScraper
-{
-	internal class ImageData
-	{
-		public List<ImageData> ListData = new List<ImageData>();
-		public string? links { get; set; }
-		public string? alts { get; set; }
-	}
-}
