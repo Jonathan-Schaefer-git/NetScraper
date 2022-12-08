8112a991f22d473d4e847747edfa254f6ff7508e
Jona4Dev <jona4play@gmail.com>
2022-12-08 22:50:32 +0100

    Pre-Rework

--- NetScraper/CoreHandler.cs
+++ NetScraper/CoreHandler.cs
@@ -1,7 +1,4 @@
 ﻿using MongoDB.Driver;
-using Newtonsoft.Json;
-using System.Diagnostics;
-using System.Text.RegularExpressions;
 
 namespace NetScraper
 {
@@ -25,7 +22,7 @@ namespace NetScraper
 			Console.WriteLine("=========================================");
 			Console.WriteLine("Type 'help' to get the list of commands");
 			var input = Console.ReadLine();
-			
+
 			switch (input)
 			{
 				default:
@@ -35,6 +32,7 @@ namespace NetScraper
 				case "help":
 					Console.WriteLine();
 					break;
+
 				case "new":
 					Console.WriteLine("Rerolling Outstanding");
 					PostgreSQL.ResetOutstanding();
@@ -49,82 +47,98 @@ namespace NetScraper
 
 			List<string> list = new List<string>();
 			list.Add("https://sn.wikipedia.org/wiki/");
+			list.Add("https://www.wikipedia.org/");
+			list.Add("https://de.wikipedia.org/");
 			PostgreSQL.PushOutstanding(list);
-			
 			StartedScraping = DateTime.Now;
 			LogWriter.WriteSettingsJson();
+
 			RunScraper();
 		}
 
-		private static void RunScraper()
+		private static async void RunScraper()
 		{
 			//Called RunScraper
 			Console.WriteLine("Called Scraping Method");
-			List<Document> documents = new List<Document>();
+
 			List<string> jsonStrings = new List<string>();
 			List<string> outstandingLinks = new List<string>();
 			List<string> prioritisedLinks = new List<string>();
 			List<string> outstanding = new List<string>();
-			
-			outstanding.AddRange(PostgreSQL.GetPrioritised());
-			Console.WriteLine(outstanding.Count());
-			outstanding.AddRange(PostgreSQL.GetOutstanding(BatchLimit - outstanding.Count));
-			var setting = LogWriter.LoadJson();
-			Shouldrun = setting.ShouldRun;
-			SimultaneousPool = setting.SimultaneousPool;
-			if (Shouldrun && outstanding != null)
-			{
-				foreach (var site in outstanding)
-				{
-					var swatch = Stopwatch.StartNew();
-					var w = Scraper.ScrapFromLink(site);
-					swatch.Stop();
-					List<Task> tasks = new List<Task>();
-					for (int i = 0; i < 100; i++)
-					{
-						tasks.Add(() => )
-					}
-					Console.WriteLine("Approximate Size for {0} is {1} and took {2}", w.Absoluteurl, w.ApproxByteSize, swatch.ElapsedMilliseconds);
-					if (w.Links != null)
-					{
-						outstandingLinks.AddRange(w.Links);
-					}
-					else
-					{
-						Console.WriteLine("No Links at this site {0}", w.Absoluteurl);
-					}
-					if (w.PrioritisedLinks != null)
-					{
-						prioritisedLinks.AddRange(w.PrioritisedLinks);
-					}
-					else
-					{
-						Console.WriteLine("No Prioritised Links at this site {0}", w.Absoluteurl);
-					}
-					Task.Run(() => PushDocumentAsync(w));
-				}
 
-				foreach (var link in outstanding)
-				{
-					outstandingLinks.Remove(link);
-					prioritisedLinks.Remove(link);
-				}
-				
-				List<string> outstandingLinksnd = new HashSet<string>(outstandingLinks).ToList();
-				List<string> prioritisedLinksnd = new HashSet<string>(prioritisedLinks).ToList();
-				
-				Console.WriteLine("Found {0} Links",outstandingLinksnd.Count);
-				Console.WriteLine("#Prioritised Links {0}", prioritisedLinksnd.Count);
-				TriggerPushLinks(outstandingLinksnd, prioritisedLinksnd);
-				Batch++;
-				Scrapes = PostgreSQL.GetScrapingCount();
-				LogWriter.WriteSettingsJson();
-			}
-			else
+			outstanding.AddRange(PostgreSQL.GetPrioritised());
+			outstanding.AddRange(PostgreSQL.GetOutstanding(BatchLimit - outstanding.Count));
+			Console.WriteLine("Outstanding Count: " + outstanding.Count());
+			while (Shouldrun)
 			{
-				Console.WriteLine("Shouldn't run or no Links to Scrap");
+				Console.WriteLine("Started Cycle");
+				var setting = LogWriter.LoadJson();
+				Shouldrun = setting.ShouldRun;
+				SimultaneousPool = setting.SimultaneousPool;
+				Console.WriteLine(SimultaneousPool);
+				if (outstanding != null)
+				{
+					var partionedLists = outstanding.Partition(SimultaneousPool);
+					Console.WriteLine("Partitioned Lists: " + partionedLists.Count());
+					foreach (var list in partionedLists)
+					{
+						var scrapingTask = await Scraping(list);
+
+						foreach (var document in scrapingTask)
+						{
+							if (document.Links != null)
+							{
+								outstandingLinks.AddRange(document.Links);
+							}
+							if (document.PrioritisedLinks != null)
+							{
+								prioritisedLinks.AddRange(document.PrioritisedLinks);
+							}
+						}
+					}
+					foreach (var link in outstanding)
+					{
+						outstandingLinks.Remove(link);
+						prioritisedLinks.Remove(link);
+					}
+
+					List<string> outstandingLinksnd = new HashSet<string>(outstandingLinks).ToList();
+					List<string> prioritisedLinksnd = new HashSet<string>(prioritisedLinks).ToList();
+
+					Console.WriteLine("Found {0} Links", outstandingLinksnd.Count);
+					Console.WriteLine("#Prioritised Links {0}", prioritisedLinksnd.Count);
+					TriggerPushLinks(outstandingLinksnd, prioritisedLinksnd);
+					Batch++;
+					Scrapes = PostgreSQL.GetScrapingCount();
+					LogWriter.WriteSettingsJson();
+				}
+				else
+				{
+					Console.WriteLine("No links to Scrap");
+				}
 			}
 		}
+		
+		private static async Task<List<Document>> Scraping(IEnumerable<string> links)
+		{
+			Console.WriteLine("Control Point A");
+			var tasks = links.Select(x => Task.Run(() => Scraper.ScrapFromLink(x))).ToArray();
+			Console.WriteLine("Control Point B");
+			await Task.WhenAll(tasks);
+			
+			
+			var documents = new List<Document>();
+			
+			foreach (var task in tasks)
+			{
+				Task.Run(() => PostgreSQL.PushDocument(task.Result));
+				documents.Add(task.Result);
+			}
+			
+			Console.WriteLine("Leaving Scraping Method");
+			return documents;
+		}
+
 		private static void TriggerPushLinks(List<string> outstandingLinks, List<string> prioritisedLinks)
 		{
 			List<Task> tasks = new List<Task>();
@@ -132,17 +146,31 @@ namespace NetScraper
 			tasks.Add(Task.Run(() => { PushPriotised(prioritisedLinks); }));
 			Task.WaitAll(tasks.ToArray());
 		}
-		private static void PushDocumentAsync(Document document)
-		{
-			PostgreSQL.PushDocument(document);
-		}
+
 		private static void PushLinks(List<string> outstandingLinks)
 		{
 			PostgreSQL.PushOutstanding(outstandingLinks);
 		}
+
 		private static void PushPriotised(List<string> prioritisedLinks)
 		{
 			PostgreSQL.PushPrioritised(prioritisedLinks);
+		}
+
+		public static IEnumerable<List<T>> Partition<T>(this IList<T> source, Int32 size)
+		{
+			for (int i = 0; i < Math.Ceiling(source.Count / (Double)size); i++)
+				yield return new List<T>(source.Skip(size * i).Take(size));
+		}
+
+		private static List<IEnumerable<string>> ListSplitter(List<string> Source, int size)
+		{
+			List<IEnumerable<string>> listOfLists = new List<IEnumerable<string>>();
+			for (int i = 0; i < Source.Count(); i += size)
+			{
+				listOfLists.Add(Source.Skip(i).Take(size));
+			}
+			return listOfLists;
 		}
 	}
 }
